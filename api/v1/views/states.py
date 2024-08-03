89058bc50a571dc0c0a8bc9c7fada7cbb92fd300
Assiminee <znatni.yasmine@gmail.com>
2024-08-03 17:49:23 +0100

    msg

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,6 +6,7 @@ Handles all default RESTFul API actions for the State object
 from api.v1.views import app_views
 from flask import request, jsonify, abort
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
@@ -25,6 +26,7 @@ def get_states(state_id=None):
 
         return jsonify(states[key].to_dict())
     
+    
     if request.method == "DELETE":
         if key not in states.keys():
             abort(404)
@@ -33,5 +35,39 @@ def get_states(state_id=None):
         storage.save()
         return jsonify({}), 200
     
-    #return jsonify([state.to_dict() for state in states.values()])
+    
+    if request.method == "POST":
+        if not request.is_json:
+            abort(400, 'Not a JSON')
+
+        req_body = request.get_json()
+
+        if 'name' not in req_body:
+            abort(400, 'Missing name')
+
+        state = State(**req_body)
+        storage.new(state)
+        storage.save()
+        return jsonify(state.to_dict()), 201
+
+    
+    if request.method == "PUT":
+        if key not in states.keys():
+            abort(404)
+
+        if not request.is_json:
+            abort(400, 'Not a JSON')
+
+        req_body = request.get_json()
+
+        state = states[key]
+
+        for key, val in req_body.items():
+            if key in ["id", "created_at", "updated_at"]:
+                continue
+            setattr(state, key, val)
+
+        storage.save()
+        return jsonify(state.to_dict()), 200
+
 
