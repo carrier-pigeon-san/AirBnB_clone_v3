4aec84c2cd9a9832acb72534d1fc9ae36debca7b
Assiminee <znatni.yasmine@gmail.com>
2024-08-05 22:09:30 +0100

    msg

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,6 +12,6 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.places import *
-from api.v1.views import cities
-from api.v1.views import users
-from api.v1.views import places_reviews
+from api.v1.views.cities import *
+from api.v1.views.users import *
+from api.v1.views.places_reviews import *
