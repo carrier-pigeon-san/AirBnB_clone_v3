d1ba4f978c3593bb6aebd6576042e2cc98eab596
Sandy Obare <obaresandy@gmail.com>
2024-08-07 03:06:19 +0300

    places_amenities.py update: conditions for handling file storage across the HTTP methods

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -17,8 +17,13 @@ def list_place_amenities(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
+    if storage_t != 'db':
+        amenities = []
+        for id in place.amenity_ids:
+            amenities.append(storage.get(Amenity, id).to_dict())
+        return jsonify(amenities), 200
     amenities = [amenity.to_dict() for amenity in place.amenities]
-    return jsonify(amenities)
+    return jsonify(amenities), 200
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
@@ -30,6 +35,10 @@ def delete_amenity(place_id, amenity_id):
     if not place or not amenity or amenity not in place.amenities:
         abort(404)
 
+    if storage_t != 'db':
+        place.amenity_ids.remove(amenity_id)
+        storage.save()
+        return jsonify({}), 200
     place.amenities.remove(amenity)
     storage.save
     return jsonify({}), 200
@@ -43,6 +52,13 @@ def create_amenity(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not place or not amenity:
         abort(404)
+
+    if storage_t != 'db':
+        if amenity_id in place.amenity_ids:
+            return jsonify(amenity.to_dict()), 200
+        place.amenity_ids.append(amenity_id)
+        storage.save()
+        return jsonify(amenity.to_dict()), 201
     if amenity in place.amenities:
         return jsonify(amenity.to_dict()), 200
     place.amenities.append(amenity)
